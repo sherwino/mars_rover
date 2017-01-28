e1900f4cf0d85f7702f143d4f65404a4f12fc03c
Sherwin Garcia-Obregon <Sherwin Garcia-Obregon>
2017-01-28 00:39:19 -0500

    Committing work to date, need to change case.

--- rover.js
+++ rover.js
@@ -34,6 +34,60 @@ var kata = {
   direction: "N",
 };
 
-console.log("Mars Rover Kata is currently facing " 
-+ kata.direction + " and is standing on point [" + kata.longlat[0] + "," + kata.longlat[1] + "]");
+var start = ("Mars Rover Kata is currently facing " + kata.direction + " and is standing on point [" + kata.longlat + "]");
+console.log(start);
+
+function move(rover) {
+  var newdir = (prompt("Which way would you like to move? w (forward), s (backward), a (left), d (right) "));
+  var updateDir = "Now your Mars Rover is facing "
+  var updateLongLat = "It was moved to..."
+
+  switch(newdir) {
+    case "w":
+    rover.direction = "N"
+    rover.longlat[0]+=1
+    updateDir += rover.direction;
+    updateLongLat += "[" + rover.longlat + "]";
+    console.log("Moving north one step")
+    console.log(updateDir);
+    console.log(updateLongLat);
+    break;
+    
+    case "s":
+    rover.longlat[0]-=1
+    rover.direction = "S"
+    updateDir += rover.direction;
+    updateLongLat += "[" + rover.longlat + "]";
+    console.log("Moving south one step")
+    console.log(updateDir);
+    console.log(updateLongLat);
+    break;
+    
+    case "a":
+    rover.longlat[1]-=1
+    rover.direction = "W"
+    updateDir += rover.direction;
+    updateLongLat += "[" + rover.longlat + "]";
+    console.log("Moving west one step")
+    console.log(updateDir);
+    console.log(updateLongLat);
+    break;
+    
+    case "d":
+    rover.longlat[1]+=1
+    rover.direction = "E"
+    updateDir += rover.direction;
+    updateLongLat += "[" + rover.longlat + "]";
+    console.log("Moving east one step")
+    console.log(updateDir);
+    console.log(updateLongLat);
+    break;
+    
+    default:
+    console.log("You didn't make any moves")
+    break;
+  };
+
+}
+
 
