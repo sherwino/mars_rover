cf7032ab082f17826b6787b2d0958fd3dc9fc14e
Sherwin Garcia-Obregon <Sherwin Garcia-Obregon>
2017-01-27 22:33:56 -0500

    Commented out the example and started my code.

--- rover.js
+++ rover.js
@@ -1,30 +1,39 @@
-var myRover = {
-  position: [0,0], 
-  direction: 'N'
-};
+// var myRover = {
+//   position: [0,0], 
+//   direction: 'N'
+// };
 
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-  };
+// function goForward(rover) {
+//   switch(rover.direction) {
+//     case 'N':
+//       rover.position[0]++
+//       break;
+//     case 'E':
+//       rover.position[1]++
+//       break;
+//     case 'S':
+//       rover.position[0]--
+//       break;
+//     case 'W':
+//       rover.position[1]--
+//       break;
+//   };
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
-}
+//   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+// }
 
-goForward(myRover);
+// goForward(myRover);
 
 // Make sure you use data structures (basic data types, arrays & objects)
 // Make sure you use boolean and conditionals
 // Make sure you use loops to avoid code repetition
 // Make sure you use functions and organize your code
+
+var kata = {
+  longlat: [0,0],
+  direction: "N",
+};
+
+console.log("Mars Rover Kata is currently facing " 
++ kata.direction + " and is standing on point [" + kata.longlat[0] + "," + kata.longlat[1] + "]");
+
